b90d14fae8fd41eb31a6b85c9fc0bf184f124faf
John1098 <hello@johnhowe.design>
2022-07-14 17:06:46 +0200

    Fixed navigation

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -3,11 +3,20 @@ import {useRouter} from 'next/router';
 import Link from 'next/link';
 
 const items = [
-  {
-    title: 'Get started',
-    links: [{href: '/docs', children: 'Overview'}],
-  },
+  { title: '', links: [{href: '/docs', children: 'Overview'}, {href: '/docs', children: 'Overview'}, {href: '/docs', children: 'Overview'}], }
 ];
+
+const itemsTwo = [
+  { title: '1. Mobilize starting your journey', links: [{href: '/docs/foreword', children: 'Foreword'}, {href: '/docs/foreword', children: 'Foreword'}],  }
+];
+
+const itemsThree = [
+  { title: '1. Mobilize starting your journey', links: [{href: '/docs/foreword', children: 'Foreword'}, {href: '/docs/foreword', children: 'Foreword'}],  }
+];
+
+
+
+
 
 export function SideNav() {
   const router = useRouter();
@@ -41,6 +50,110 @@ export function SideNav() {
             overflow-y: auto;
             padding: 2.5rem 2rem 2rem;
             border-right: 1px solid var(--border-color);
+          }
+          span {
+            font-size: larger;
+            font-weight: 500;
+            padding: 0.5rem 0 0.5rem;
+          }
+          ul {
+            padding: 0;
+          }
+          li {
+            list-style: none;
+            margin: 0;
+          }
+          li a {
+            text-decoration: none;
+          }
+          li a:hover,
+          li.active > a {
+            text-decoration: underline;
+          }
+        `}
+      </style>
+      {itemsTwo.map((item) => (
+        <div className='navTwo' key={item.title}>
+          <span>{item.title}</span>
+          <ul className="flex column">
+            {item.links.map((link) => {
+              const active = router.pathname === link.href;
+              return (
+                <li key={link.href} className={active ? 'active' : ''}>
+                  <Link {...link}>
+                    <a href={link.href}>{link.children}</a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ))}
+      <style jsx>
+        {`
+          nav {
+            position: sticky;
+            top: var(--top-nav-height);
+            height: calc(100vh - var(--top-nav-height));
+            flex: 0 0 auto;
+            overflow-y: auto;
+            padding: 2.5rem 2rem 2rem;
+            border-right: 1px solid var(--border-color);
+          }
+          .navTwo {
+            background-color: #1b4098;
+          }
+          span {
+            font-size: larger;
+            font-weight: 500;
+            padding: 0.5rem 0 0.5rem;
+          }
+          ul {
+            padding: 0;
+          }
+          li {
+            list-style: none;
+            margin: 0;
+          }
+          li a {
+            text-decoration: none;
+          }
+          li a:hover,
+          li.active > a {
+            text-decoration: underline;
+          }
+        `}
+      </style>
+      {itemsThree.map((item) => (
+        <div className='navThree' key={item.title}>
+          <span>{item.title}</span>
+          <ul className="flex column">
+            {item.links.map((link) => {
+              const active = router.pathname === link.href;
+              return (
+                <li key={link.href} className={active ? 'active' : ''}>
+                  <Link {...link}>
+                    <a href={link.href}>{link.children}</a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ))}
+      <style jsx>
+        {`
+          nav {
+            position: sticky;
+            top: var(--top-nav-height);
+            height: calc(100vh - var(--top-nav-height));
+            flex: 0 0 auto;
+            overflow-y: auto;
+            padding: 2.5rem 2rem 2rem;
+            border-right: 1px solid var(--border-color);
+          }
+          .navThree {
+            background-color: #d3d3d3;
           }
           span {
             font-size: larger;
---
+++ pages/docs/foreword.md
@@ -0,0 +1,5 @@
+---
+title: Foreword
+---
+
+# {% $markdoc.frontmatter.title %}
